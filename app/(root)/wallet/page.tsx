21bde68324e16ce2aca24e71f61db751d40d16cb
SantinoGiordano <santinogiordano13@gmail.com>
2025-10-16 11:18:16 -0400

    created page for purchases

---
+++ app/(root)/wallet/page.tsx
@@ -0,0 +1,8 @@
+export default function Wallet(){
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>Wallet Page
+            </h1>
+        </main>
+    )   
+}
