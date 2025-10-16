b8ab25c2104b66aea6e32a02ab435860b54f1ed3
SantinoGiordano <santinogiordano13@gmail.com>
2025-10-16 11:18:03 -0400

    created page for budget

---
+++ app/(root)/budget/page.tsx
@@ -0,0 +1,8 @@
+export default function Budget(){
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>Budget Page
+            </h1>
+        </main>
+    )   
+}
