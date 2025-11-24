e6b3c9826683dc834c4fccb92bdc6baea9074a22
SantinoGiordano <santinogiordano13@gmail.com>
2025-11-24 10:09:13 -0500

    spellchecker

--- app/(root)/budget/page.tsx
+++ app/(root)/budget/page.tsx
@@ -1,7 +1,7 @@
 export default function Budget(){
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>Budget Page
+            <h1>Set my Bugdets
             </h1>
         </main>
     )   
